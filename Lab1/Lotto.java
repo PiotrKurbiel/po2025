75001745cb4d35ec943dac77bee57da642299b88
pkurbiel <piotrkurbiel05@gmail.com>
2025-10-13 17:49:31 +0200

    Losowanie do suckesu

--- Lab1/Lotto.java
+++ Lab1/Lotto.java
@@ -3,27 +3,38 @@ import java.util.Arrays;
 import java.util.Random;
 public class Lotto {
     public static void main(String[] args) {
-        ArrayList liczby = new ArrayList(6);
-        Random random = new Random();
-        for (int i = 0; i < 6; i++) {
-            int losowa = random.nextInt(1,51);
-            if(liczby.contains(losowa)){
-                i--;
-                continue;
-            }
-            liczby.add(losowa);
 
-        }
-        System.out.println("Liczby wylosowane przez system: " + liczby);
+        Random random = new Random();
         System.out.println("Liczby wybrane przez ciebie: " + Arrays.asList(args));
         int hit = 0;
-        for(int i = 0; i < 6; i++){
-            if(liczby.contains(Integer.parseInt(args[i]))){
-                hit++;
+        int ilosclos = 0;
+        long czasprzed = System.currentTimeMillis();
+        while(hit != 6) {
+            hit = 0;
+            ArrayList liczby = new ArrayList(6);
+            for (int i = 0; i < 6; i++) {
+                int losowa = random.nextInt(1, 51);
+                if (liczby.contains(losowa)) {
+                    i--;
+                    continue;
+                }
+                liczby.add(losowa);
+
             }
+            //System.out.println("Liczby wylosowane przez system: " + liczby);
+
+            for (int i = 0; i < 6; i++) {
+                if (liczby.contains(Integer.parseInt(args[i]))) {
+                    hit++;
+                }
+            }
+            //System.out.println("Trafiłeś " + hit + " liczb");
+            //System.out.println();
+            ilosclos++;
         }
-        System.out.println("Trafiłeś " + hit + " liczb");
-        System.out.println();
+        long czaspo = System.currentTimeMillis();
+        System.out.println("Ilosc losowan do trafienia " + ilosclos);
+        System.out.println("Czas do trafienia w sekundach " + (czaspo - czasprzed)/1000.0);
 
     }
 }
