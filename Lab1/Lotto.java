0142c03a696574a4531d5d14ecd9499dc471cbaa
pkurbiel <piotrkurbiel05@gmail.com>
2025-10-13 17:23:41 +0200

    Lotto wpisywanie argumentow

--- Lab1/Lotto.java
+++ Lab1/Lotto.java
@@ -1,11 +1,12 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Random;
 public class Lotto {
     public static void main(String[] args) {
         ArrayList liczby = new ArrayList(6);
         Random random = new Random();
         for (int i = 0; i < 6; i++) {
-            int losowa = random.nextInt(0,51);
+            int losowa = random.nextInt(1,51);
             if(liczby.contains(losowa)){
                 i--;
                 continue;
@@ -13,8 +14,16 @@ public class Lotto {
             liczby.add(losowa);
 
         }
-
-        System.out.println(liczby);
+        System.out.println("Liczby wylosowane przez system: " + liczby);
+        System.out.println("Liczby wybrane przez ciebie: " + Arrays.asList(args));
+        int hit = 0;
+        for(int i = 0; i < 6; i++){
+            if(liczby.contains(Integer.parseInt(args[i]))){
+                hit++;
+            }
+        }
+        System.out.println("Trafiłeś " + hit + " liczb");
+        System.out.println();
 
     }
 }
